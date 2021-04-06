49ab49764f8d33041dc01939722a3f7ca8ec2282
Maurizio <maurizio.conza@cazoo.co.uk>
2021-04-06 10:54:00 +0100

    mob next [ci-skip]

--- src/kata/golf/hole3/takehomecalculator.ts
+++ src/kata/golf/hole3/takehomecalculator.ts
@@ -1,45 +1,43 @@
-import {Incalculable} from "./incalculable";
+import { Incalculable } from "./incalculable";
 
 export class Money {
-    public value: number;
-    public currency: string;
+  public value: number;
+  public currency: string;
 
+  constructor(value: number, currency: string) {
+    this.value = value;
+    this.currency = currency;
+  }
 
-    constructor(value: number, currency: string) {
-        this.value = value;
-        this.currency = currency;
+  plus(other: Money): Money {
+    if (other.currency !== this.currency) {
+      throw new Incalculable();
     }
+    return new Money(this.value + other.value, other.currency);
+  }
 }
 
 export class Takehomecalculator {
-    private percent: number
+  private percent: number;
 
-    constructor(percent: number) {
-        this.percent = percent;
+  constructor(percent: number) {
+    this.percent = percent;
+  }
+
+  netAmount(first: Money, ...rest: Money[]): Money {
+    const monies: Array<Money> = Array.from(rest);
+    let total: Money = first;
+
+    for (const next of monies) {
+      total = total.plus(next);
     }
 
-    netAmount(first: Money, ...rest : Money[] ): Money {
+    const amount: number = total.value * (this.percent / 100.0);
+    const tax: Money = new Money(Math.trunc(amount), first.currency);
 
-        const monies: Array<Money> = Array.from(rest);
-        let total: Money = first
-
-        for (let next of monies) {
-            if (next.currency !== total.currency) {
-                throw new Incalculable()
-            }
-        }
-
-        for (const next of monies) {
-            total = new Money(total.value + next.value, next.currency)
-        }
-
-        const amount:number = total.value * (this.percent / 100.0 );
-        const tax: Money = new Money(Math.trunc(amount), first.currency);
-
-        if (total.currency !== tax.currency) {
-            throw new Incalculable()
-        }
-        return new Money(total.value - tax.value, first.currency)
+    if (total.currency !== tax.currency) {
+      throw new Incalculable();
     }
-
+    return new Money(total.value - tax.value, first.currency);
+  }
 }
